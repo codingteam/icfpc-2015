10fa05f378485c04db015327fb93315362904775
Ilya Portnov <portnov@bk.ru>
2015-08-08 10:32:52 +0500

    Basic data definition for Field.

--- src/main/scala/ru/org/codingteam/icfpc/Emulator.scala
+++ src/main/scala/ru/org/codingteam/icfpc/Emulator.scala
@@ -18,8 +18,12 @@ abstract class Command
 case class Move(direction : Direction.Direction)
 case class Turn(clockwise : Boolean)
 
-object Field {
-  
+class Field(width : Int, height : Int) {
+  var field = Array.ofDim[CellState.CellState](width, height)
+
+  def apply(x : Int, y : Int) : CellState.CellState = {
+    return field(x)(y)
+  }
 }
 
 object Emulator {

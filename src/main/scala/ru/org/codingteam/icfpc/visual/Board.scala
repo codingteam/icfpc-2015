b4b2eab871880ca445b9771d19dddfdcf7efb96e
rexim <reximkut@gmail.com>
2015-08-08 13:55:04 +0600

    Some sort of integrated with the Emulator

--- src/main/scala/ru/org/codingteam/icfpc/visual/Board.scala
+++ src/main/scala/ru/org/codingteam/icfpc/visual/Board.scala
@@ -4,8 +4,8 @@ import java.awt.{Color, Graphics2D, Graphics}
 import javax.swing.JPanel
 
 
-class Board(width: Int, height: Int) extends JPanel {
-  private val cells = Array.fill(height, width)(Color.WHITE)
+class Board(val rowsCount: Int, val colsCount: Int) extends JPanel {
+  private val cells = Array.fill(rowsCount, colsCount)(Color.WHITE)
 
   override def paintComponent(g: Graphics): Unit = {
     val g2 = g.asInstanceOf[Graphics2D]
@@ -15,26 +15,26 @@ class Board(width: Int, height: Int) extends JPanel {
 
 
   private def renderGrid(g2: Graphics2D): Unit = {
-    val cellWidth = getWidth / width.toFloat
-    val cellHeight = getHeight / height.toFloat
+    val cellWidth = getWidth / colsCount.toFloat
+    val cellHeight = getHeight / rowsCount.toFloat
 
     g2.setColor(Color.BLACK)
 
-    for (i <- 1 until width) {
+    for (i <- 1 until colsCount) {
       g2.drawLine((i * cellWidth).toInt, 0, (i * cellWidth).toInt, getHeight)
     }
 
-    for (i <- 1 until height) {
+    for (i <- 1 until rowsCount) {
       g2.drawLine(0, (i * cellHeight).toInt, getWidth, (i * cellHeight).toInt)
     }
   }
 
   private def renderCells(g2: Graphics2D): Unit = {
-    val cellWidth = getWidth / width.toFloat
-    val cellHeight = getHeight / height.toFloat
+    val cellWidth = getWidth / colsCount.toFloat
+    val cellHeight = getHeight / rowsCount.toFloat
 
-    for (row <- 0 until height) {
-      for (col <- 0 until width) {
+    for (row <- 0 until rowsCount) {
+      for (col <- 0 until colsCount) {
         g2.setColor(cells(row)(col))
         g2.fillRect((col * cellWidth).toInt, (row * cellHeight).toInt,
           cellWidth.toInt, cellHeight.toInt)
@@ -42,7 +42,7 @@ class Board(width: Int, height: Int) extends JPanel {
     }
   }
 
-  def putCell(x: Int, y: Int, color: Color): Unit = {
-    cells(x)(y) = color
+  def putCell(row: Int, col: Int, color: Color): Unit = {
+    cells(row)(col) = color
   }
 }

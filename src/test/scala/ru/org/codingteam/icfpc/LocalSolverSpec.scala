d059c854cc8b1e73b0c45456a7b3edd89c56d4d7
Alexander Batischev <eual.jp@gmail.com>
2015-08-09 13:42:28 +0300

    Add test for LocalSolver

---
+++ src/test/scala/ru/org/codingteam/icfpc/LocalSolverSpec.scala
@@ -0,0 +1,16 @@
+package ru.org.codingteam.icfpc
+
+import org.scalatest.{Matchers, FlatSpec}
+import ru.org.codingteam.icfpc.definitions._
+
+class LocalSolverSpec extends FlatSpec with Matchers {
+    "LocalSolver" should "find straight path in problem_0" in {
+        val unit = UnitDef(Vector(CellDef(0, 0)), CellDef(0,0))
+        val field = new Field(10, 10)
+        val pos = (4, 9)
+        val solution = LocalSolver.findPath(field, unit, pos)
+        // there are a number of equivalent solutions, so I'll just test for
+        // the cost
+        assert(solution.get.length === 9)
+    }
+}

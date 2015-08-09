07351b486a51edf742309487336759efdfe6689a
Friedrich von Never <friedrich@fornever.me>
2015-08-09 21:05:22 +0600

    Fix post script.

--- Post-Submissions.ps1
+++ Post-Submissions.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem *.solution.json | % {
-    $command = "curl --% --user :mlzBHPsKo5ANdYQaj73UsluHpl39mCCEidAf7obtTjk= -X POST -H `"Content-Type: application/json`" -d @$_  https://davar.icfpcontest.org/teams/83/solutions --insecure"
+    $command = "curl --user :mlzBHPsKo5ANdYQaj73UsluHpl39mCCEidAf7obtTjk= -X POST -H `"Content-Type: application/json`" -d @$_  https://davar.icfpcontest.org/teams/83/solutions --insecure"
 
     Write-Host $command
     cmd /c $command

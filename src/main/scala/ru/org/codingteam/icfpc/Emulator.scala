codingteam/icfpc-2015

commit bbc1bdf9e0cb8b0bd28c259fdfef6c08793755be

Implement basic LocalSolver
* Incapable of rotating the figures
* No maps of reachability - will eat your CPU like a hog!
* Bonus: bugs, bugs, bugs everywhere! Tests coming real soon now.

src/main/scala/ru/org/codingteam/icfpc/Emulator.scala
@@ -214,13 +214,19 @@ class Emulator (val field : Field) {
     result
   }
 
-  // return true if it is possible to spawn new unit
-  def spawnUnit(unit : UnitDef) : Boolean = {
+
+  def getSpawnPositionShifts(unit: UnitDef): (Int, Int) = {
     val unitSize = Utils.getUnitActualSize(unit)
     val cY = - unit.members.map(_.y).min
     val minX = unit.members.map(_.x).min
     val cX = (field.width - unitSize._1) / 2 - minX
     //println(s"$cX = (${field.width} - ${unitSize._1}) / 2 - $minX")
+    (cX, cY)
+  }
+
+  // return true if it is possible to spawn new unit
+  def spawnUnit(unit : UnitDef) : Boolean = {
+    val (cX, cY) = getSpawnPositionShifts(unit)
     val translated = translate(unit)(cX, cY)
     currentUnit = translated
     return check(currentUnit)
@@ -271,16 +277,19 @@ class Emulator (val field : Field) {
       (pvtY + clockwiseCoeff * rotated.y).toInt)
   }
 
-  // Return true if the unit is locked as a result of command execution
-  def executeCommand(cmd : Command) : Boolean = {
-    currentUnit = cmd match {
+  // Return true if the unit will be locked as a result of command execution
+  def willLock(unit: UnitDef, cmd : Command) : Boolean = {
+    val result = cmd match {
                     case Move(direction) =>
-                      mapUnit(currentUnit)(Emulator.translateCoord(direction))
+                      mapUnit(unit)(Emulator.translateCoord(direction))
                     case Turn(clockwise) =>
-                      mapUnit(currentUnit)(rotateCoord(clockwise))
+                      mapUnit(unit)(rotateCoord(clockwise))
                   }
-    return ! check(currentUnit)
+    return ! check(result)
   }
+
+  // Return true if the unit is locked as a result of command execution
+  def executeCommand(cmd : Command) : Boolean = willLock(currentUnit, cmd)
 
   // Lock current unit: mark all corresponding cells as Full
   def lock(unit : UnitDef) : Unit = {

src/main/scala/ru/org/codingteam/icfpc/LocalSolver.scala
@@ -2,11 +2,16 @@ package ru.org.codingteam.icfpc
 
 import ru.org.codingteam.icfpc.definitions.{UnitDef,CellDef}
 import ru.org.codingteam.icfpc.Solver.Position
+import ru.org.codingteam.icfpc.Utils._
 import scala.math.{floor,ceil,signum,max,abs}
-import scala.collection.mutable.PriorityQueue
+import scala.collection.mutable.{PriorityQueue,HashSet}
 
 case class Solution(pathCost: Long, estimatedCost: Long, position: Position,
-    commands: List[Command])
+    commands: List[Command]) {
+
+    // that's how I want it to be for HashSet to work
+    override def hashCode = position.hashCode
+}
 
 object LocalSolver {
     def findPath(field: Field, unit: UnitDef, pos: Position):
@@ -19,13 +24,59 @@ object LocalSolver {
                 .compare(y.pathCost + y.estimatedCost)
         }
 
-        val openset: PriorityQueue[Solution] = new PriorityQueue()
+        // need our own instance so that we can call getSpawnPosition
+        val emul: Emulator = new Emulator(field)
 
-        None
+        val openset: PriorityQueue[Solution] = new PriorityQueue()
+        val closedset: HashSet[Solution] = new HashSet()
+        val (cX, cY) = emul.getSpawnPositionShifts(unit)
+        val spawnPosition: Position = ( unit.pivot.x + cX,
+                                        unit.pivot.y + cY )
+        openset.enqueue(new Solution(0,
+                                     distance(spawnPosition, pos),
+                                     spawnPosition,
+                                     List()))
+
+        var solution: Solution = null
+
+        while (!openset.isEmpty && solution == null) {
+            val current = openset.dequeue
+            if(current.position == pos) {
+                solution = current
+            } else {
+                closedset.add(current)
+
+                // extend current solution with each possible command and add
+                // the result to openset
+
+                def go(dir: Direction.Direction): Solution = {
+                    val new_pos = emul.translateCoord(dir)(current.position._1,
+                                                           current.position._2)
+                    new Solution(current.pathCost + 1,
+                                 distance(new_pos, pos),
+                                 new_pos,
+                                 Move(dir) :: current.commands)
+                }
+
+                val translated = emul.translate(unit)(current.position._1,
+                                                      current.position._2)
+
+                if(!emul.willLock(translated, Move(Direction.E)))
+                    openset.enqueue(go(Direction.E))
+                if(!emul.willLock(translated, Move(Direction.W)))
+                    openset.enqueue(go(Direction.W))
+                if(!emul.willLock(translated, Move(Direction.SE)))
+                    openset.enqueue(go(Direction.SE))
+                if(!emul.willLock(translated, Move(Direction.SW)))
+                    openset.enqueue(go(Direction.SW))
+            }
+        }
+
+        if(solution == null) None else Some(solution.commands)
     }
 
     /* Distance between two cells */
-    private def distance(a: CellDef, b: CellDef): Int = {
+    private def distance(a: (Int, Int), b: (Int, Int)): Int = {
         /* As per section 5 of
          * http://www-cs-students.stanford.edu/~amitp/Articles/HexLOS.html
          */
@@ -33,8 +84,8 @@ object LocalSolver {
         def floor2(x: Int): Int = if (x >= 0) (x/2).toInt else ((x-1)/2).toInt
         def ceil2(x: Int): Int = if (x >= 0) ((x+1)/2).toInt else (x/2).toInt
 
-        val dx = (b.x - floor2(b.y) - (a.x - floor2(a.y))).toInt
-        val dy = (b.x + ceil2(b.y) - (a.x + ceil2(a.y))).toInt
+        val dx = (b._1 - floor2(b._2) - (a._1 - floor2(a._2))).toInt
+        val dy = (b._1 + ceil2(b._2) - (a._1 + ceil2(a._2))).toInt
         if (signum(dx) == signum(dy)) {
             max(abs(dx), abs(dy));
         } else {

commit 49c17de0f46f8d920bbf500dc9e36795743f26ea

Make `executeCommand` mutatate `currentUnit`
That fixes an error introduced in
bbc1bdf9e0cb8b0bd28c259fdfef6c08793755be

src/main/scala/ru/org/codingteam/icfpc/Emulator.scala
@@ -277,19 +277,22 @@ class Emulator (val field : Field) {
       (pvtY + clockwiseCoeff * rotated.y).toInt)
   }
 
+  def tryCommand(unit: UnitDef, cmd: Command): UnitDef = cmd match {
+    case Move(direction) =>
+      mapUnit(unit)(Emulator.translateCoord(direction))
+    case Turn(clockwise) =>
+      mapUnit(unit)(rotateCoord(clockwise))
+    }
+
   // Return true if the unit will be locked as a result of command execution
-  def willLock(unit: UnitDef, cmd : Command) : Boolean = {
-    val result = cmd match {
-                    case Move(direction) =>
-                      mapUnit(unit)(Emulator.translateCoord(direction))
-                    case Turn(clockwise) =>
-                      mapUnit(unit)(rotateCoord(clockwise))
-                  }
-    return ! check(result)
-  }
+  def willLock(unit: UnitDef, cmd : Command) : Boolean =
+      ! check(tryCommand(unit, cmd))
 
   // Return true if the unit is locked as a result of command execution
-  def executeCommand(cmd : Command) : Boolean = willLock(currentUnit, cmd)
+  def executeCommand(cmd : Command) : Boolean = {
+      currentUnit = tryCommand(currentUnit, cmd)
+      return ! check(currentUnit)
+  }
 
   // Lock current unit: mark all corresponding cells as Full
   def lock(unit : UnitDef) : Unit = {
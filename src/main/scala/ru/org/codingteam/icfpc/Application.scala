e9623ee21db75d2b485067f2a8bb67e0730be768
Ilya Portnov <portnov@bk.ru>
2015-08-09 19:26:34 +0500

    Implement output to file: sbt ... -output output.json.

--- src/main/scala/ru/org/codingteam/icfpc/Application.scala
+++ src/main/scala/ru/org/codingteam/icfpc/Application.scala
@@ -1,5 +1,6 @@
 package ru.org.codingteam.icfpc
 
+import java.io.PrintStream
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -14,6 +15,7 @@ case class Arguments(fields: Seq[FieldDef],
                      memoryLimit: Int,
                      cores: Int,
                      phrases: Set[String],
+                     outFile : Option[String],
                      print: Boolean,
                      scoresFile : Option[String])
 
@@ -35,9 +37,10 @@ object Application extends App {
     val phrases = arguments.getOrElse("-p", List()).toList
     val print = arguments.get("-print")
     val scoresFile = arguments.get("-scores").map(_.head)
+    val outFile = arguments.get("-output").map(_.head)
 
     val fields = (filenames map parseFile).toVector
-    Arguments(fields, timeLimit.seconds, memory, cores, Set(phrases: _*), print.isDefined, scoresFile)
+    Arguments(fields, timeLimit.seconds, memory, cores, Set(phrases: _*), outFile, print.isDefined, scoresFile)
   }
 
   def parseFile(fileName: String): FieldDef = {
@@ -85,6 +88,10 @@ object Application extends App {
   val arguments = parseArgs()
   arguments.fields foreach { field =>
     val solutions = solve(field, arguments.phrases, arguments.print, arguments.scoresFile)
-    println(Serializer.serialize(solutions) + "\n")
+    val output = arguments.outFile match {
+      case None => System.out
+      case Some(path) => new PrintStream(path)
+    }
+    output.println(Serializer.serialize(solutions) + "\n")
   }
 }

65f562d0071ddff63fcebbc6537c56ff1aceda9b
Friedrich von Never <friedrich@fornever.me>
2015-08-09 21:00:47 +0600

    Add submission scripts.

---
+++ Post-Submissions.ps1
@@ -0,0 +1,6 @@
+Get-ChildItem *.solution.json | % {
+    $command = "curl --% --user :mlzBHPsKo5ANdYQaj73UsluHpl39mCCEidAf7obtTjk= -X POST -H `"Content-Type: application/json`" -d @$_  https://davar.icfpcontest.org/teams/83/solutions --insecure"
+
+    Write-Host $command
+    cmd /c $command
+}
